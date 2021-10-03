ec8e71491cb784a3f84f2cd1735231c971031151
Prime <jeff.d.937@gmail.com>
2021-10-03 18:48:50 -0400

    'moved my table of contents here'

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+   
+    
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +16,31 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return '# ${data.title}
+  ## License:
+  '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
 
-`;
-}
+  ## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+              1.[GitHub](#githubgithub.com/DeonJeff)
+              2.[Email](#emailwwwjeff.d.937@gmail.com)
+
+## Description
+## Installation
+## Usage
+## License
+## Contributing
+## Tests
+## Questions
+### [GitHub](github.com/DeonJeff)
+### [Email](http://www.jeff.d.937@gmail.com)
+  
 
 module.exports = generateMarkdown;
